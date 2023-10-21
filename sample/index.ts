889cbb5908b66ac11f23c7cfad93f581fc14e953
umingming <u_0112@naver.com>
2023-10-21 22:19:14 +0900

    Define variable type

--- sample/index.ts
+++ sample/index.ts
@@ -1,1 +1,24 @@
-let 이름 = 'kim';
+//대부분 알아서 해줌.
+let myName = '유미';
+//myName = 3; 
+let myAge :number = 28;
+let myCity :string = "수원";
+let hasJob :boolean = true;
+let members :string[] = ['kim', 'park'];
+//다른 타입이 들어갔을 때 에러처리해주는 것.
+let member :{name :string, age :number} = {
+    name: "이유미",
+    age: 28
+}
+type song = {
+    [key : string] : string
+}
+const myFavoriteSong :song = {
+    title: "wonderwall",
+    singer: "Oasis"
+}
+type project = {
+    member : string[],
+    days : number,
+    started : boolean
+}
