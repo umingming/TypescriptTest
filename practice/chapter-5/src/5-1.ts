7d6640c28169375936c2bb98e25d65c739ec9e8b
umingming <u_0112@naver.com>
2024-10-29 22:31:42 +0900

    test: Abstract class

--- practice/chapter-5/src/5-1.ts
+++ practice/chapter-5/src/5-1.ts
@@ -4,11 +4,37 @@ type Rank = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 
 class Position {
   constructor(private file: FileType, private rank: Rank) {}
+
+  distanceFrom(position: Position) {
+    return {
+      rank: Math.abs(position.rank - this.rank),
+      file: Math.abs(position.file.charCodeAt(0) - this.file.charCodeAt(0)),
+    };
+  }
 }
 
-class Piece {
+abstract class Piece {
   protected position: Position;
   constructor(private readonly color: Color, file: FileType, rank: Rank) {
     this.position = new Position(file, rank);
+  }
+
+  moveTo(position: Position) {
+    this.position = position;
+  }
+  abstract canMoveTo(position: Position): boolean;
+}
+
+class King extends Piece {
+  canMoveTo(position: Position) {
+    let distance = this.position.distanceFrom(position);
+    return distance.rank < 2 && distance.file < 2;
+  }
+}
+
+class Game {
+  private pieces = Game.makePieces();
+  private static makePieces() {
+    return [new King("White", "E", 1), new King("Black", "E", 8)];
   }
 }
