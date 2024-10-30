bafb7ff6dae712d513488da7065fb9c751be9897
umingming <u_0112@naver.com>
2024-10-30 23:02:27 +0900

    feat: Define user interface

--- practice/chapter-5/src/5-1.ts
+++ practice/chapter-5/src/5-1.ts
@@ -3,7 +3,7 @@ type FileType = "A" | "B" | "C" | "D" | "E" | "F" | "G" | "H";
 type Rank = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8;
 
 class Position {
-  constructor(private file: FileType, private rank: Rank) {}
+  constructor(private file: FileType, private rank: Rank, index = 0) {}
 
   distanceFrom(position: Position) {
     return {
@@ -11,8 +11,9 @@ class Position {
       file: Math.abs(position.file.charCodeAt(0) - this.file.charCodeAt(0)),
     };
   }
-}
 
+  log() {}
+}
 abstract class Piece {
   protected position: Position;
   constructor(private readonly color: Color, file: FileType, rank: Rank) {
@@ -37,4 +38,12 @@ class Game {
   private static makePieces() {
     return [new King("White", "E", 1), new King("Black", "E", 8)];
   }
+}
+
+// interface User {
+//   age: number;
+// }
+
+interface User<Age extends number> {
+  age: Age;
 }
