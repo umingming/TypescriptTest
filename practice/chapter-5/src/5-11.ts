8dc876422286d9b3dbef3b2a0288b5c46f57d563
umingming <u_0112@naver.com>
2024-10-31 19:20:16 +0900

    feat: Define factory pattern

--- practice/chapter-5/src/5-11.ts
+++ practice/chapter-5/src/5-11.ts
@@ -1,4 +1,5 @@
 /*
+  프록시
  */
 
 interface Proxy {
@@ -26,3 +27,39 @@ proxy.print();
 // console.log(proxy instanceof Proxy);
 // console.log(proxy instanceof Target);
 // console.log(proxy instanceof ProxyTarget);
+
+/*
+  팩토리
+*/
+
+type Shoe = {
+  purpose: string;
+};
+
+class BalletFlat implements Shoe {
+  purpose = "dancing";
+}
+
+class Boot implements Shoe {
+  purpose = "woodcutting";
+}
+
+class Sneaker implements Shoe {
+  purpose = "walking";
+}
+
+// 타입스크립트는 값과 타입의 네임스페이스를 따로 관리하기 때문에 같은 이름으로 선언 가능하다
+let Shoe = {
+  create(type: "balletFlat" | "boot" | "sneaker"): Shoe {
+    switch (type) {
+      case "balletFlat":
+        return new BalletFlat();
+      case "boot":
+        return new Boot();
+      case "sneaker":
+        return new Sneaker();
+    }
+  },
+};
+
+const boot = Shoe.create("boot");
